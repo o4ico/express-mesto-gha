29452ec587c04c028eccf45bc9ce06367dea3a4f
Darya Sokolova <o4ico@yandex.ru>
2023-08-15 14:57:28 +0300

    fix: исправления для прохождения автотестов

--- controllers/cards.js
+++ controllers/cards.js
@@ -2,7 +2,6 @@ const Card = require('../models/card');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
-    .populate('user')
     .then(cards => res.status(200).send(cards))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
 };
