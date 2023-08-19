5410926df75cb462ffad6b23292119506597c29d
Darya Sokolova <o4ico@yandex.ru>
2023-08-19 23:34:48 +0300

    fix: авторизация

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,22 +2,27 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = require('../utils/constants');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
-  let payload;
-
   try {
-    payload = jwt.verify(token, SECRET_KEY);// верифицируем токен
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+      return res.status(401).send({ message: 'Необходима авторизация' });
+    }
+
+    const token = authorization.replace('Bearer ', '');
+    let payload;
+
+    try {
+      payload = jwt.verify(token, SECRET_KEY);// верифицируем токен
+    } catch (err) {
+      return res.status(401).send({ message: 'Необходима авторизация' });
+    }
+
+    req.user = payload; // записываем пейлоуд в объект запроса
+
+    next(); // пропускаем запрос дальше
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(err);
   }
 
-  req.user = payload; // записываем пейлоуд в объект запроса
-
-  next(); // пропускаем запрос дальше
 };
