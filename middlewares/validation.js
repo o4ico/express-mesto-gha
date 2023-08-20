a7f78aa0246396adc1dce961627e9f16461d284a
Darya Sokolova <o4ico@yandex.ru>
2023-08-20 14:28:00 +0300

    fix: исправление работоспособности валидации полей

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -1,52 +1,53 @@
 const { celebrate, Joi } = require('celebrate');
 linkValid = (/http:\/\/(.+?)\/(([a-zA-Z0-9_ \-%\.]*)\.(jpg|png|jpeg|gif))/);
+const { ValidationError } = require('../errors/ValidationError');
 
 const userIdValidation = celebrate({
   params: Joi.object().keys({
-    userId: Joi.string().length(24).hex().required().error(new Error('Переданы некорректные данные')),
+    userId: Joi.string().length(24).hex().required(),
   }),
 });
 
 const userInfoEditValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).error(new Error('Переданы некорректные данные')),
-    about: Joi.string().min(2).max(30).error(new Error('Переданы некорректные данные')),
+    name: Joi.string().min(2).max(30).messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    about: Joi.string().min(2).max(30).messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
 const userAvatarEditValidation = celebrate({
   body: Joi.object().keys({
-    avatar: Joi.string().regex(linkValid).error(new Error('Переданы некорректные данные')),
+    avatar: Joi.string().regex(linkValid).messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
 const cardIdValidation = celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex().required().error(new Error('Переданы некорректные данные')),
+    cardId: Joi.string().length(24).hex().required().messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
 const cardCreateValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).required().error(new Error('Переданы некорректные данные')),
-    link: Joi.string().regex(linkValid).required().error(new Error('Переданы некорректные данные')),
+    name: Joi.string().min(2).max(30).required().messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    link: Joi.string().regex(linkValid).required().messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
 const loginValidation = celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email().error(new Error('Переданы некорректные данные')),
-    password: Joi.string().required().min(8).error(new Error('Переданы некорректные данные')),
+    email: Joi.string().required().email().messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    password: Joi.string().required().min(8).messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
 const createUserValidation = celebrate({
   body: Joi.object().keys({
-    name: Joi.string().min(2).max(30).error(new Error('Переданы некорректные данные')),
-    about: Joi.string().min(2).max(30).error(new Error('Переданы некорректные данные')),
-    avatar: Joi.string().regex(linkValid).error(new Error('Переданы некорректные данные')),
-    email: Joi.string().required().email().error(new Error('Переданы некорректные данные')),
-    password: Joi.string().required().min(8).error(new Error('Переданы некорректные данные')),
+    name: Joi.string().min(2).max(30).messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    about: Joi.string().min(2).max(30).messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    avatar: Joi.string().regex(linkValid).messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    email: Joi.string().required().email().messages({ 'ValidationError': 'Переданы некорректные данные' }),
+    password: Joi.string().required().min(8).messages({ 'ValidationError': 'Переданы некорректные данные' }),
   }),
 });
 
